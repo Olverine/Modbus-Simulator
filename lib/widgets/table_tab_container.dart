84e2522c97ec4802681f477fda04cd483892919d
Oliver Norin <oliver.david.norin@gmail.com>
2024-10-17 15:57:45 +0200

    first commit

---
+++ lib/widgets/table_tab_container.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:modbus_simulator/widgets/register_table.dart';
+import 'package:tab_container/tab_container.dart';
+
+class TableTabContainer extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).height,
+      child: TabContainer(
+        borderRadius: BorderRadius.circular(20),
+        tabEdge: TabEdge.top,
+        curve: Curves.easeIn,
+        transitionBuilder: (child, animation) {
+          animation = CurvedAnimation(
+              curve: Curves.easeIn, parent: animation);
+          return SlideTransition(
+            position: Tween(
+              begin: const Offset(0.2, 0.0),
+              end: const Offset(0.0, 0.0),
+            ).animate(animation),
+            child: FadeTransition(
+              opacity: animation,
+              child: child,
+            ),
+          );
+        },
+        selectedTextStyle: const TextStyle(fontSize: 18),
+        unselectedTextStyle: const TextStyle(fontSize: 16),
+        tabs: _getTabs(),
+        children: _getChildren(),
+      ),
+    );
+  }
+
+  List<Widget> _getTabs() {
+    return [
+      const Text("Discrete Output Coils"),
+      const Text("Discrete Input Contacts"),
+      const Text("Analog Input Registers"),
+      const Text("Analog Output Holding Registers"),
+    ];
+  }
+
+  List<Widget> _getChildren() {
+    return [
+      const Text("Under construction"),
+      const Text("Under construction"),
+      RegisterTable(),
+      const Text("Under construction"),
+    ];
+  }
+}
